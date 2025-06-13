320ee982655a87ba1bec8e9fde87818e7be9d555
tm-xtreme <teamxtrm.bd@gmail.com>
2025-06-13 17:08:35 +0600

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+// Import Firebase core and services
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMxsru5wYDPQQMflZNuSRovgL3dpSAtsQ",
+  authDomain: "moonusdt.firebaseapp.com",
+  projectId: "moonusdt",
+  storageBucket: "moonusdt.firebasestorage.app",
+  messagingSenderId: "250779626021",
+  appId: "1:250779626021:web:4a622a9664715963b3adcc",
+  measurementId: "G-E8XXHB12L7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export initialized services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
